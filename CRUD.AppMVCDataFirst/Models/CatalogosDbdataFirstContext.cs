f20943740e2b9ec75a61efc6f8958ef9e1edb4f6
emelyyy03 <emelyyy03@outlook.com>
2025-03-09 18:58:43 -0600

    Migracion y configuracion del program y appJson

---
+++ CRUD.AppMVCDataFirst/Models/CatalogosDbdataFirstContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.AppMVCDataFirst.Models;
+
+public partial class CatalogosDbdataFirstContext : DbContext
+{
+    public CatalogosDbdataFirstContext()
+    {
+    }
+
+    public CatalogosDbdataFirstContext(DbContextOptions<CatalogosDbdataFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC071BA3ADA2");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- CRUD.AppMVCDataFirst/Program.cs
+++ CRUD.AppMVCDataFirst/Program.cs
@@ -1,7 +1,16 @@
+using CRUD.AppMVCDataFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CatalogosDbdataFirstContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
