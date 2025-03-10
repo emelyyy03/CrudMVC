0a8e0edf674d2d73080bd18f2e85edca07529369
emelyyy03 <emelyyy03@outlook.com>
2025-03-09 18:27:44 -0600

    Agregar archivos de proyecto.

---
+++ CRUD.AppMVCCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD.AppMVCCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
